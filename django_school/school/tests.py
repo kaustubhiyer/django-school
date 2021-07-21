d01f43a47f56257b2d09fedbdddbf7f45b05fafc
Kaustubh Mac <kaustubhiyer1998@gmail.com>
2021-07-21 10:50:05 +0530

    Unit tests

--- django_school/school/tests.py
+++ django_school/school/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+import json
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import path, reverse
 
-# Create your tests here.
+class StudentTestCase(APITestCase):
+  def setUp(self):
+    data = { "name": "Teacher", "gender": "M", "email": "test", "classroom": []}
+    url = reverse("teacher_create")
+    response = self.client.post(url, data, format="json")
+
+  def test_create(self):
+    data = { "name": "Test Student", "gender": "M", "grade": 96, "teacher": 1 }
+    url = reverse('student_create')
+    response = self.client.post(url, data, format="json")
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
